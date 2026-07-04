0cd1559a137a95188f2240e1ae8de2962ba5eee4
Jordan Paramo <jordan.param1@gmail.com>
2026-07-04 05:04:39 -0500

    fix: Fix AI Integration
    *  Do not tell Mason to install copilot-language-server
       *  Already bundled by copilot.lua
    *  Use Sidekick to integrate with ACP coding agents (thanks Folke!)
    *  Use Sidekick and copilot-lsp for Next Edit Suggestions
    *  Remove code companion
    *  Integrate Blink and NES
    *  Update docs

--- lua/config/plugins/ai/copilot.lua
+++ lua/config/plugins/ai/copilot.lua
@@ -1,33 +1,33 @@
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	event = "InsertEnter",
-	opts = {
-		filetypes = {
-			dockerfile = true,
-			markdown = true,
-			terraform = true,
-			javascript = true,
-			typescript = true,
-			proto = true,
-			python = true,
-			cpp = true,
-			csharp = true,
-			go = true,
-			lua = true,
-			rust = true,
-			sh = function()
-				if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
-					return false
-				end
-				return true
-			end,
-			["*"] = false,
-		},
-		suggestion = { enabled = false },
-		panel = { enabled = false },
-		nes = {
-			enabled = false,
-		},
-	},
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  opts = {
+    filetypes = {
+      dockerfile = true,
+      markdown = true,
+      terraform = true,
+      javascript = true,
+      typescript = true,
+      proto = true,
+      python = true,
+      cpp = true,
+      csharp = true,
+      go = true,
+      lua = true,
+      rust = true,
+      sh = function()
+        if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
+          return false
+        end
+        return true
+      end,
+      ["*"] = false,
+    },
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+    nes = {
+      enabled = true,
+    },
+  },
 }
--- lua/config/plugins/lsp/mason.lua
+++ lua/config/plugins/lsp/mason.lua
@@ -1,36 +1,36 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		require("mason").setup()
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    require("mason").setup()
 
-		require("mason-lspconfig").setup({
-			automatic_installation = true,
-			ensure_installed = {
-				"cssls",
-				"eslint",
-				"html",
-				"jsonls",
-				"ts_ls",
-				"pyright",
-				"tailwindcss",
-				"gopls",
-				"rust_analyzer",
-			},
-		})
+    require("mason-lspconfig").setup({
+      automatic_installation = true,
+      ensure_installed = {
+        "cssls",
+        "eslint",
+        "gopls",
+        "html",
+        "jsonls",
+        "lua_ls",
+        "pyright",
+        "rust_analyzer",
+        "tailwindcss",
+        "ts_ls",
+      },
+    })
 
-		require("mason-tool-installer").setup({
-			ensure_installed = {
-				"prettier",
-				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
-				"pylint",
-				"eslint_d",
-			},
-		})
-	end,
+    require("mason-tool-installer").setup({
+      ensure_installed = {
+        "prettier",
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint",
+        "eslint_d",
+      },
+    })
+  end,
 }
